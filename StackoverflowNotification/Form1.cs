c82821a052cd043b7179bcf2eab4746f320886f2
Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2017-04-18 21:53:29 +0600

    Started the project

--- StackoverflowNotification/Form1.cs
+++ StackoverflowNotification/Form1.cs
@@ -10,11 +10,51 @@ using System.Windows.Forms;
 
 namespace StackoverflowNotification
 {
+    using StackExchange.StacMan;
+
+    using Tulpep.NotificationWindow;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            var popupNotifier = new PopupNotifier();
+            var client  = new StacManClient();
+
+
+            var response = client.Questions.GetAll("stackoverflow",
+            page: 1,
+            pagesize: 10,
+            tagged:"sql-server",
+            order: Order.Desc).Result;
+
+            StringBuilder content = new StringBuilder();
+
+            int i = 0;
+
+            foreach (var question in response.Data.Items)
+            {
+                content.Append($"{++i}. {question.Title}\n");
+            }
+
+
+            popupNotifier.TitleText = "Recent Stackoverflow Post";
+            popupNotifier.BodyColor = Color.GhostWhite;
+            popupNotifier.BorderColor = Color.DarkRed;
+            popupNotifier.HeaderColor = Color.CadetBlue;
+
+            popupNotifier.ContentText = content.ToString();
+            popupNotifier.IsRightToLeft = false;
+            popupNotifier.Size = new Size(500, 20*i);
+            popupNotifier.Popup();
         }
     }
 }
