2f836503b4d7f47391118409ede35251d8294dd6
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2017-04-20 00:50:24 +0600

    Update design and refactor code

--- StackoverflowNotification/FormSettings.Designer.cs
+++ StackoverflowNotification/FormSettings.Designer.cs
@@ -29,68 +29,78 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonEnd = new System.Windows.Forms.Button();
             this.timer = new System.Windows.Forms.Timer(this.components);
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // buttonStart
             // 
-            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(25, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Start";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.ButtonStartClick);
+            this.buttonStart.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(12, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(88, 34);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStartClick);
             // 
-            // button2
+            // buttonEnd
             // 
-            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(25, 52);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(88, 33);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Stop";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.ButtonStopClick);
+            this.buttonEnd.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEnd.Location = new System.Drawing.Point(12, 52);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(88, 33);
+            this.buttonEnd.TabIndex = 1;
+            this.buttonEnd.Text = "Stop";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.ButtonStopClick);
             // 
-            // comboBox1
+            // textBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "sql-server",
-            "php"});
-            this.comboBox1.Location = new System.Drawing.Point(221, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(135, 21);
-            this.comboBox1.TabIndex = 2;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(135, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 23);
+            this.textBox1.TabIndex = 4;
             // 
-            // label1
+            // button3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(134, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 18);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Select Tag";
+            this.button3.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(278, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Search a tag";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.ButtonSearchClick);
+            // 
+            // trackBar
+            // 
+            this.trackBar.Location = new System.Drawing.Point(135, 61);
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(104, 45);
+            this.trackBar.TabIndex = 6;
+            this.trackBar.Scroll += new System.EventHandler(this.TrackBarScroll);
             // 
             // FormSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(400, 190);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.buttonStart);
             this.Name = "FormSettings";
             this.ShowIcon = false;
             this.Text = "Stackoverflow Notification";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,11 +108,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonEnd;
         private System.Windows.Forms.Timer timer;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar;
     }
 }
 
--- StackoverflowNotification/FormSettings.cs
+++ StackoverflowNotification/FormSettings.cs
@@ -9,6 +9,7 @@
 namespace StackoverflowNotification
 {
     using System;
+    using System.Diagnostics;
     using System.Drawing;
     using System.Linq;
     using System.Text;
@@ -20,24 +21,43 @@ namespace StackoverflowNotification
 
     public partial class FormSettings : Form
     {
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int Tag { get; set; }
+        public int PopupDelay { get; set; }
+        public int TimerInterval { get; set; }
+
+
+        PopupNotifier popupNotifier = new PopupNotifier();
+
+
         public FormSettings()
         {
+
             this.InitializeComponent();
-            this.Interval = 5000;
+            //this.InitializeTracBar();
+            this.InitializePopupNotifierBasicSettings();
+
+            // Setting default values
+            this.TimerInterval = 5000;
+            this.PopupDelay = 2000;
+            Debug.WriteLine("Initializing formSetting with timeInterval = 5 and popupdelay = 2");
         }
 
-        public int Interval { get; set; }
 
         private void ButtonStartClick(object sender, EventArgs e)
         {
-            this.button1.Enabled = false;
-            this.timer.Interval = this.Interval;
+            this.buttonStart.Enabled = false;
             this.timer.Enabled = true;
+            this.timer.Interval = this.TimerInterval;
+            Debug.WriteLine("ButtonStart click before timer tick");
             this.timer.Tick += new EventHandler(this.OnTimerEvent);
+            Debug.WriteLine("ButtonStart click after timer tick");
         }
 
         public StacManResponse<Question> FetchNotification()
         {
+            Debug.WriteLine("FetchNotification method called");
             return
                 new StacManClient().Questions.GetAll(
                     "stackoverflow",
@@ -49,6 +69,7 @@ namespace StackoverflowNotification
 
         public void DisplayPopup(StacManResponse<Question> notificationMessage)
         {
+            Debug.WriteLine("Popup delay methosd called");
             var i = 0;
             var popupText = new StringBuilder();
             var questions = notificationMessage.Data.Items;
@@ -57,35 +78,66 @@ namespace StackoverflowNotification
                 popupText.AppendLine($"{i++}. {question.Title} [Link]");
             }
 
-            var popupNotifier = new PopupNotifier
-                                    {
-                                        TitleText = "Recent Stackoverflow Post",
-                                        BodyColor = Color.GhostWhite,
-                                        BorderColor = Color.DarkRed,
-                                        HeaderColor = Color.CadetBlue,
-                                        ContentText = popupText.ToString(),
-                                        Delay = 5000,
-                                        Size = new Size(500, questions.Count() * 22)
-                                    };
+            //var popupNotifier = new PopupNotifier
+            //                        {
+            //                            TitleText = "Recent Stackoverflow Post",
+            //                            BodyColor = Color.GhostWhite,
+            //                            BorderColor = Color.DarkRed,
+            //                            HeaderColor = Color.CadetBlue,
+            //                            ContentText = popupText.ToString(),
+            //                            Delay = 5000,
+            //                            Size = new Size(500, questions.Count() * 22)
+            //                        };
+            popupNotifier.TitleText = "Recent Stackoverflow Post";
+            popupNotifier.ContentText = popupText.ToString();
+            popupNotifier.Delay = this.PopupDelay;
+            popupNotifier.Size = new Size(500, questions.Count() * 22);
 
             popupNotifier.Popup();
         }
 
+        public void InitializePopupNotifierBasicSettings()
+        {
+            this.popupNotifier.BodyColor = Color.GhostWhite;
+            this.popupNotifier.BorderColor = Color.DarkRed;
+            this.popupNotifier.HeaderColor = Color.CadetBlue;
+        }
+
         public void OnTimerEvent(object source, EventArgs e)
         {
+            Debug.WriteLine("Ontimer event method called");
             var fetchedMessage = this.FetchNotification();
             this.DisplayPopup(fetchedMessage);
         }
 
         private void ButtonStopClick(object sender, EventArgs e)
         {
-            if (this.button1.Enabled)
+            if (this.buttonStart.Enabled)
             {
                 return;
             }
             this.timer.Stop();
-            this.button1.Enabled = true;
+            this.buttonStart.Enabled = true;
         }
 
+        private void ButtonSearchClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackBarScroll(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"tracbar value set to {trackBar.Value}");
+            this.PopupDelay = this.trackBar.Value;
+        }
+
+        public void InitializeTracBar()
+        {
+            Debug.WriteLine($"Initialize tracbar called");
+            this.trackBar.Maximum = 300;
+            this.trackBar.Minimum = 1;
+            this.trackBar.TickFrequency = 150;
+
+        }
     }
 }
